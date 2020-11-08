ec8db2336a73dd14f675c15531e63b11f5079b2c
mzi207 <mzi207@nyu.edu>
2020-11-07 19:53:52 -0500

    Added frontend basics
    Added Notes and sidebar basics to help start incorporating frontend design with backend design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React, {useState} from 'react';
-import Notes from './Notes'
+import React from 'react';
+import SidebarComponent from './Sidebar/Sidebar';
+import NoteComponent from './Notes/Notes';
+import './App.css';
 
-function App() {
-  return (
-    <>
-      <Notes/>
-    </>
-  )
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      /* TO-DO 
+        Add a filePath once folders are made
+        Add an owner and contributors when accounts registration are made
+        Add Revision History (This sounds very complicated could delete)
+        Add Tags once folders are made
+        Add Themes (last feature) for aethetic purposes
+      */
+      fileID: null,
+      fileName: null,
+      notes: null, //
+
+    }
+  }
+  render(){
+    return (
+      <div class="notation-container">
+        <SidebarComponent
+          fileID={this.state.fileID}
+          notes={this.state.notes}></SidebarComponent>
+        <NoteComponent></NoteComponent>
+
+      </div>
+    )
+  }
+  /*
+  componentDidMount = () =>{
+    //Connect the database with this
+  }
+  */
 }
 
 export default App;
