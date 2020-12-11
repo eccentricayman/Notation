857223ea369b4ea8f4e2b807994cbbcdc069ca4e
mzi207 <mzi207@nyu.edu>
2020-12-11 02:47:33 -0500

    Added option of canvas or quill
    Pick between either canvas or quill, also cleaner way to add notes

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -3,7 +3,18 @@ import { withStyles} from '@material-ui/core/styles';
 import styles from './styles';
 import List from '@material-ui/core/List';
 import {Divider, Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import SidebarItemComponent from '../SidebarItem/SidebarItem';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 
 class SidebarComponent extends React.Component{
@@ -19,17 +30,43 @@ class SidebarComponent extends React.Component{
         return(
             <div className={classes.sidebarContainer}>
                 <Button
-                    onClick={this.newNoteBtnClick}
-                    className={classes.newNoteBtn}>{this.state.addingNote ? "Cancel" : "New Note"}
+                    onClick={this.addingNote}
+                    className={classes.addingNote}>{"Add Note"}
                 </Button>
                 {
                     this.state.addingNote ? 
                     <div>
-                        <input type="text" className={classes.newNoteFileName} placeholder="Enter Note Name"
-                        onKeyUp={(e) => this.updateFileName(e.target.value)}></input>
-                        <Button
-                            className={classes.newNoteSubmitBtn}
-                            onClick={this.newNote}>Submit</Button>
+                        <Dialog open={this.state.addingNote} onClose={this.closingNote} aria-labelledby="form-dialog-title">
+                            <DialogTitle id="form-dialog-title">Adding a new Note</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    To add a new note please input a file name and select which note taking application would you like to use, Quill or Canvas
+                                </DialogContentText>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="note Name"
+                                    label="File Name"
+                                    type="text"
+                                    fullWidth
+                                />
+                                <FormControl component="fieldset">
+                                <FormLabel component="legend">Note Type: </FormLabel>
+                                <RadioGroup aria-label="noteType" name="noteType" >
+                                    <FormControlLabel value="true" control={<Radio />} label="Quill" />
+                                    <FormControlLabel value="false" control={<Radio />} label="Canvas" />
+                                </RadioGroup>
+                                </FormControl>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={this.closingNote} color="primary">
+                                    Cancel
+                                </Button>
+                                <Button onClick={this.newNote, this.closingNote} color="primary">
+                                    Submit
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
                     </div> : null
                 }
                 <List>
@@ -72,15 +109,19 @@ class SidebarComponent extends React.Component{
     </List>
     */
     //Creates a blank new Note File
-    newNoteBtnClick = () => {
-        this.setState({addingNote:!this.state.addingNote, title: null});
+    addingNote = () => {
+        this.setState({addingNote:!this.state.addingNote});
+    }
+    closingNote = () => {
+        this.setState({addingNote:!this.state.addingNote});
     }
     updateFileName = (name) => {
         this.setState({title: name});
     }
     newNote = () => {
         //Insert database storing here
-        this.setState({title:null, addingNote: false});
+        //this.setState({title:null, addingNote: false});
+        console.log("cool database stuff here")
     }
     selectNote = () =>{
         console.log("Select Note")
