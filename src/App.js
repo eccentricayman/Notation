f98455706b83ae0140e94f7d1bccc92f84513392
Ayman Ahmed <eccentricayman@macbook-pro.lan>
2020-12-11 07:23:00 -0500

    authentication working now, signout button doesnt wanna show up thuogh

--- src/App.js
+++ src/App.js
@@ -2,54 +2,88 @@ import React from 'react';
 import SidebarComponent from './Sidebar/Sidebar';
 import NoteComponent from './Notes/Notes';
 import './App.css';
-import Register from './components/auth/Register.js';
-import Login from './components/auth/Login.js';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+//import Register from './components/auth/Register.js';
+//import Login from './components/auth/Login.js';
+
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
+
 /*
-function App() {
+  function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+  <div className="App">
+  <Register />
+  </div>
   );
 */
-class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      /* TO-DO 
-        Add a filePath once folders are made
-        Add an owner and contributors when accounts registration are made
-        Add Tags once folders are made
-        Add Themes (last feature) for aethetic purposes
-      */
-      fileID: null,
-      fileName: null,
-      selectedNoteIndex: null,
-      selectedNote: null,
-      notes: [{title: "Testing Title", body: "abcdefghijklmnopqrstuvwxyz"}], //
 
-    }
-  }
-  render(){
-    return (
-      <div className="notation-container">
-        {/* <AmplifySignOut /> */}
-        <SidebarComponent
-          fileID={this.state.fileID}
-          notes={this.state.notes}
-          selectedNoteIndex={this.state.selectedNoteIndex}></SidebarComponent>
-			<NoteComponent></NoteComponent>
-			<AmplifySignOut />
-
-      </div>
-    )
-  }
-  /*
-  componentDidMount = () =>{
-    //Connect the database with this
-  }
-  */
+class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			/* TO-DO 
+			  Add a filePath once folders are made
+			  Add an owner and contributors when accounts registration are made
+			  Add Tags once folders are made
+			  Add Themes (last feature) for aethetic purposes
+			*/
+			fileID: null,
+			fileName: null,
+			selectedNoteIndex: null,
+			selectedNote: null,
+			notes: [{title: "Testing Title", body: "abcdefghijklmnopqrstuvwxyz"}], //
+			
+		};
+	}
+	render(){
+	    //	if (this.props.authState == "signedIn") {
+		return (
+			<AmplifyAuthenticator usernameAlias="email">
+                <AmplifySignUp
+                    slot="sign-up"
+                    usernameAlias="email"
+                    formFields={[
+                        {
+                            type: "name",
+                            label: "Name",
+                            placeholder: "McLovin",
+                            required: true,
+                        },
+                        {
+                            type: "email",
+                            label: "Email",
+                            placeholder: "notationgang@example.com",
+                            required: true,
+                        },
+                        {
+                            type: "password",
+                            label: "Password",
+                            placeholder: "********",
+                            required: true,
+                        },
+                    ]} 
+                />
+                <AmplifySignIn slot="sign-in" usernameAlias="email" />
+                
+				<div className="notation-container">
+					<SidebarComponent
+				        fileID={this.state.fi1leID}
+				        notes={this.state.notes}
+				        selectedNoteIndex={this.state.selectedNoteIndex}>             
+                    </SidebarComponent>
+					<NoteComponent></NoteComponent>
+                    <AmplifySignOut/>
+				</div>
+                
+			</AmplifyAuthenticator>
+		);
+		//}
+	}
+	/*
+	  componentDidMount = () =>{
+	  //Connect the database with this
+	  }
+	*/
 }
 
-export default withAuthenticator(App);
+export default App;
+//export default withAuthenticator(App, { usernameAttributes: 'email', signUpConfig, includeGreetings: true });
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -8,6 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import {makeStyles} from '@material-ui/core/styles';
+import { Auth } from 'aws-amplify';
 
 
 const useStyles = makeStyles((theme) => ({
