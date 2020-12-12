234b51b8ab50173a4c744de7c2d61f76b8a30016
Brian Lu <45515803+bl2456@users.noreply.github.com>
2020-12-11 19:53:59 -0500

    added local delete

--- src/App.js
+++ src/App.js
@@ -51,7 +51,16 @@ class App extends React.Component{
 
   selectNote = (note, index) => this.setState({selectedNoteIndex: index, selectedNote: note});
 
-  deleteNote = (note) => console.log("delete note");
+  deleteNote = async (note) => {
+    const noteIndex = this.state.notes.indexOf(note);
+    await this.setState({
+      notes: this.state.notes.filter( (aNote) => aNote !== note)
+    });
+    if(this.state.selectedNoteIndex === noteIndex){
+      this.setState({selectedNoteIndex: null, selectedNote: null});
+    } 
+    
+  }
 
   updateNote = (noteObject) => {
     // console.log(noteObject);
