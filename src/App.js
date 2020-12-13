39df3ac09f5fb4deb697c3ca61306bc6661b2baa
Ayman Ahmed <aa6149@nyu.edu>
2020-12-12 20:12:40 -0500

    updated with full backend support; tag support wonky

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
 import { API, Auth } from 'aws-amplify';
 import { listNotes, getNote } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation, updateNote as updateNoteMutation } from './graphql/mutations';
 
 
 /*
@@ -20,70 +20,72 @@ import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } fr
   );
 */
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      /* TO-DO 
-        Add a filePath once folders are made
-        Add an owner and contributors when accounts registration are made
-        Add Tags once folders are made
-        Add Themes (last feature) for aethetic purposes
-      */
-      fileID: null,
-      title: null,
-      selectedNoteIndex: null,
-      selectedNote: null,
-      notes: [{id: 1, title: "Testing Title", data: "<p>abcdefghijklmnopqrstuvwxyz</p>", type: 1, tags: ['omegatag'], owner: 'random'},
-      {id: 2, title: "Testing Title 2", data: "<p>Hello world</p>", type: 1, tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6'], owner: 'random2'}], //
-      currentNewId: 3,
-      //True for react Quill, False for React-Canvas, default is false
-      quill:true
-    }
-  }
-  // addNote = async (title, data, type) => {
-  //   let id = this.state.currentNewId;
-  //   console.log(id);
-  //   await this.setState(prevState => ({
-  //     notes: [...prevState.notes, {id: id, title: title, data: data, type: type}],
-  //   }));
-  //   //console.log(this.state.notes);
-  //   let noteIndex = this.state.notes.findIndex((note) => note.id === id);
-  //   //console.log(noteIndex);
-  //   id++;
-  //   console.log(id);
-  //   this.setState({selectedNote: this.state.notes[noteIndex], selectedNoteIndex: noteIndex, currentNewId: id})
-  //   //console.log(this.state.notes);
-  //   if (!type && !this.state.quill){
-  //     this.noteComponent.clear();
-  //   }
+	constructor(){
+		super();
+		this.state = {
+			/* TO-DO 
+			  Add a filePath once folders are made
+			  Add an owner and contributors when accounts registration are made
+			  Add Tags once folders are made
+			  Add Themes (last feature) for aethetic purposes
+			*/
+			fileID: null,
+			title: null,
+			selectedNoteIndex: null,
+			selectedNote: null,
+			notes: [{id: 1, title: "Testing Title", data: "<p>abcdefghijklmnopqrstuvwxyz</p>", type: 1, tags: ['omegatag'], owner: 'random'},
+					{id: 2, title: "Testing Title 2", data: "<p>Hello world</p>", type: 1, tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6'], owner: 'random2'}], //
+			currentNewId: 3,
+			//True for react Quill, False for React-Canvas, default is false
+			quill:true
+		};
+	}
+	// addNote = async (title, data, type) => {
+	//   let id = this.state.currentNewId;
+	//   console.log(id);
+	//   await this.setState(prevState => ({
+	//     notes: [...prevState.notes, {id: id, title: title, data: data, type: type}],
+	//   }));
+	//   //console.log(this.state.notes);
+	//   let noteIndex = this.state.notes.findIndex((note) => note.id === id);
+	//   //console.log(noteIndex);
+	//   id++;
+	//   console.log(id);
+	//   this.setState({selectedNote: this.state.notes[noteIndex], selectedNoteIndex: noteIndex, currentNewId: id})
+	//   //console.log(this.state.notes);
+	//   if (!type && !this.state.quill){
+	//     this.noteComponent.clear();
+	//   }
 
     //need to figure out how to get notes to use this function
     fetchNotes = async () => {
         const apiData = await API.graphql({ query: listNotes });
-        this.setState({notes: [...this.state.notes, apiData]});
+        this.setState({notes: this.state.notes.concat(apiData['data']['listNotes']['items'])});
     }
-    
+
+	//printNotes = async () => {
+	//const apiData = await API.graphql({ query: listNotes });
+	//console.log(apiData);
+	//}
+	
 	addNote = async (title, data, type) => {
-        let id = this.state.currentNewId;
-        console.log(id);
-        //let note = {id: "%d"%id, title: title, data: data, type: type, tags: [], owner: Auth.user.username};
-        let note = {id: id, title: title, data: data, type: type, tags: [], owner: Auth.user.username};
+        //let id = this.state.currentNewId;
+        //console.log(id);
+        let note = {title: title, data: data, type: type, tags: [], owner: Auth.user.username};
+		let postedNote = await API.graphql({ query: createNoteMutation, variables: { input: note } });
         await this.setState(prevState => ({
-            notes: [...prevState.notes, note],
+            notes: [...prevState.notes, postedNote['data']['createNote']],
         }));
-        
-        //await API.graphql({ query: createNoteMutation, variables: { input: note } });
-        
+        //console.log(postedNote);
+        let id = postedNote['data']['createNote']['id'];
         //console.log(this.state.notes);
         let noteIndex = this.state.notes.findIndex((note) => note.id === id);
         //console.log(noteIndex);
-        id++;
-        console.log(id);
         this.setState({selectedNote: this.state.notes[noteIndex], selectedNoteIndex: noteIndex, currentNewId: id});
         //console.log(this.state.notes);
         if (!type && !this.state.quill){
             this.noteComponent.clear();
-        }
+		}
     }
     
     selectNote = (note, index) => {
@@ -92,6 +94,7 @@ class App extends React.Component{
             this.noteComponent.clear();
         }
     }
+	
     deleteNote = async (note) => {
         const noteIndex = this.state.notes.indexOf(note);
         await this.setState({
@@ -99,8 +102,11 @@ class App extends React.Component{
         });
         if(this.state.selectedNoteIndex === noteIndex){
             this.setState({selectedNoteIndex: null, selectedNote: null});
-        } 
-        
+        }
+		const toDelete = {
+			id: note['id'],
+		};
+        await API.graphql({ query: deleteNoteMutation, variables: { input: toDelete } });
     }
     
     updateNote = (noteObject) => {
@@ -116,35 +122,62 @@ class App extends React.Component{
         noteToUpdate.type = noteObject.type;
         notes[index] = noteToUpdate;
         this.setState({notes: notes});
+		const updateMap = {
+			id: noteToUpdate.id,
+			title: noteToUpdate.title,
+			data: noteToUpdate.data,
+			type: noteToUpdate.type
+		};
+		API.graphql({ query: updateNoteMutation, variables: { input: updateMap } });
     };
     
     addTag = (noteId, newTag) => {
-      console.log(noteId, newTag);
-      let notes = [...this.state.notes];
-      console.log(notes);
-      const index = notes.findIndex((note) => note.id === noteId);
-      console.log(index);
-      console.log(this.state.notes[index]);
-      const noteToUpdate = {...this.state.notes[index]};
-      console.log(noteToUpdate);
-      const updatedTags = [...noteToUpdate.tags, newTag];
-      noteToUpdate.tags = updatedTags;
-      notes[index] = noteToUpdate;
-      this.setState({notes: notes});
+		//console.log(noteId, newTag);
+		let notes = [...this.state.notes];
+		console.log(notes);
+		const index = notes.findIndex((note) => note.id === noteId);
+		console.log(index);
+		console.log(this.state.notes[index]);
+		const noteToUpdate = {...this.state.notes[index]};
+		console.log(noteToUpdate);
+		const updatedTags = [...noteToUpdate.tags, newTag];
+		noteToUpdate.tags = updatedTags;
+		notes[index] = noteToUpdate;
+		this.setState({notes: notes});
+		const updateMap = {
+			id: noteToUpdate.id,
+            tags: updatedTags
+		};
+        API.graphql({ query: updateNoteMutation, variables: { input: updateMap } });
     }
 
     deleteTag = (noteId,targetTag) => {
-      let notes = [...this.state.notes];
-      const index = notes.findIndex((note) => note.id === noteId);
-      const noteToUpdate = {...this.state.notes[index]};
-      const newTags = noteToUpdate.tags.filter( (tag) => tag !== targetTag);
-      noteToUpdate.tags = newTags;
-      notes[index] = noteToUpdate;
-      this.setState({notes: notes});
+		let notes = [...this.state.notes];
+		const index = notes.findIndex((note) => note.id === noteId);
+		const noteToUpdate = {...this.state.notes[index]};
+		const newTags = noteToUpdate.tags.filter( (tag) => tag !== targetTag);
+		noteToUpdate.tags = newTags;
+		notes[index] = noteToUpdate;
+		this.setState({notes: notes});
+        const updateMap = {
+			id: noteToUpdate.id,
+            tags: newTags
+		};
+        API.graphql({ query: updateNoteMutation, variables: { input: updateMap } });
     }
+    
     shareNote = (note,user) => {
-      console.log(user);
+		console.log(user);
     }
+
+	componentDidMount() {
+		this.fetchNotes();
+	}
+
+	//componentDidChange() {
+	//this.printNotes();
+	//}
+	
 	render(){
 	    //	if (this.props.authState == "signedIn") {
         console.log(this.state);
@@ -177,32 +210,32 @@ class App extends React.Component{
                 <AmplifySignIn slot="sign-in" usernameAlias="email" />
                 
 				<div className="notation-container">
-          <div id="side-menu">
-            <AmplifySignOut/>
-          </div>
-          <SidebarComponent
-            setNoteType={this.setNoteType}
-            fileID={this.state.fileID}
-            notes={this.state.notes}
-            selectedNoteIndex={this.state.selectedNoteIndex}
-            addNote={this.addNote}
-            selectNote={this.selectNote}
-            deleteNote={this.deleteNote}
-            addTag={this.addTag}
-            deleteTag={this.deleteTag}
-            shareNote={this.shareNote}></SidebarComponent>
-          { this.state.selectedNote ?
-            <NoteComponent 
-              ref={noteComponent =>this.noteComponent = noteComponent}
-              quill = {this.state.quill}
-              selectedNote={this.state.selectedNote}
-              selectedNoteIndex={this.state.selectedNoteIndex}
-              notes={this.state.notes}
-              updateNote={this.updateNote}></NoteComponent> :
-              <div className="none-selected">Please choose a note to view or create a new one!</div>
-          }
-            
-				</div>
+				    <div id="side-menu">
+				        <AmplifySignOut/>
+				    </div>
+				    <SidebarComponent
+                        setNoteType={this.setNoteType}
+                        fileID={this.state.fileID}
+                        notes={this.state.notes}
+                        selectedNoteIndex={this.state.selectedNoteIndex}
+                        addNote={this.addNote}
+                        selectNote={this.selectNote}
+                        deleteNote={this.deleteNote}
+                        addTag={this.addTag}
+                        deleteTag={this.deleteTag}
+                        shareNote={this.shareNote}></SidebarComponent>
+				    { this.state.selectedNote ?
+				      <NoteComponent 
+				          ref={noteComponent =>this.noteComponent = noteComponent}
+				          quill = {this.state.quill}
+				          selectedNote={this.state.selectedNote}
+				          selectedNoteIndex={this.state.selectedNoteIndex}
+				          notes={this.state.notes}
+				          updateNote={this.updateNote}></NoteComponent> :
+				      <div className="none-selected">Please choose a note to view or create a new one!</div>
+				    }
+                    
+			    </div>
                 
 			</AmplifyAuthenticator>
 		);
